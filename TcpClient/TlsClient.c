1295254df99812b9dd7f2e630a994907d5ef9557
Jan Seredynski <janseredynski@gmail.com>
2022-01-07 02:04:56 +0100

    Enable SSL Pinning

--- TcpClient/TlsClient.c
+++ TcpClient/TlsClient.c
@@ -62,6 +62,10 @@ int tlsHanshake(int fd){
 //  BIO * bio = BIO_new_ssl_connect(ctx);
 //  BIO_set_conn_hostname(bio, hostname_port_c);
   ssl = SSL_new(ctx);
+  
+  // Set SSL Pinning
+  CORE_HttpClient_loadDefaultCerts(ctx);
+  
 //  int fd_from_tcp_client = server;
   SSL_set_fd(ssl, fd);
   
@@ -77,18 +81,23 @@ int tlsHanshake(int fd){
   X509_NAME       *certname = NULL;
   
   cert = SSL_get_peer_certificate(ssl);
-  if (cert == NULL) printf("");
-//      BIO_printf(outbio, "Error: Could not get a certificate from: %s.\n", dest_url);
-  else printf("");
-//      BIO_printf(outbio, "Retrieved the server's certificate from: %s.\n", dest_url);
-
-    /* ---------------------------------------------------------- *
-     * extract various certificate information                    *
-     * -----------------------------------------------------------*/
-    certname = X509_NAME_new();
-    certname = X509_get_subject_name(cert);
-
+  if (cert == NULL) {
+    printf("No certificate found");
+    return -1;
+  }
+  char buf_subject[256];
+  char buf_issuer[256];
+  char *cert_subject = X509_NAME_oneline(X509_get_subject_name(cert), buf_subject, sizeof(buf_subject));
+  char *cert_issuer = X509_NAME_oneline(X509_get_issuer_name(cert), buf_issuer, sizeof(buf_issuer));
+  printf("certname=%s, issuer=%s", buf_subject, buf_issuer);
   
+
+  long ssl_pinning_res = SSL_get_verify_result(ssl);
+  if( ssl_pinning_res != X509_V_OK){
+      printf("SSL Pinner: Bad OK");
+      return -1;
+  }
+  printf("SSL Pinner: Cert OK");
   return 0;
 }
 
@@ -100,11 +109,11 @@ void CORE_HttpClient_loadCert(char pem[], X509_STORE *cert_store) {
     printf("Imported Certificate: %s\n",X509_NAME_oneline(X509_get_subject_name(pinned_cert), NULL, 0));
 }
 
-char CORE_cert_app_root[];
+char cert_root_lets_encrypt[];
 void CORE_HttpClient_loadDefaultCerts(SSL_CTX *ctx){
     X509_STORE * cert_store = X509_STORE_new();
-    CORE_HttpClient_loadCert(CORE_cert_app_root, cert_store);
-    // load_cert(inter_ca_cert1, cert_store); // just load more to add more
+    CORE_HttpClient_loadCert(cert_root_lets_encrypt, cert_store);
+//    load_cert(inter_ca_cert1, cert_store); // just load more to add more
     SSL_CTX_set_cert_store(ctx, cert_store);
 }
 
@@ -149,6 +158,39 @@ char CORE_cert_app_root[] =
 "E6nMkcLatZdIVnPP2mGCcPSHd8M+AP+9msqzjSKyHwPs+d1luZAs4W4xTSUHSncn\n"
 "BJyA2BHrmcI7qVDcB1F3UZK3GzLrSp27cvY4nM3EtVZtsJ/vzFWV75nEKMq4Qbhl\n"
 "SN1k\n"
+"-----END CERTIFICATE-----";
+
+char cert_root_lets_encrypt[] =
+"-----BEGIN CERTIFICATE-----\n"
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n"
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n"
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n"
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n"
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n"
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n"
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n"
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n"
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n"
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n"
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n"
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n"
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n"
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n"
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n"
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n"
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n"
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n"
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n"
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n"
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n"
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n"
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n"
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n"
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n"
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n"
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n"
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n"
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n"
 "-----END CERTIFICATE-----";
 
 
@@ -222,3 +264,5 @@ int create_socket(char url_str[], BIO *out) {
 
   return sockfd;
 }
+
+
